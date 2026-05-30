788261829a79273f4746ea475f9fce850f73262b
ZQDQ <823984418@qq.com>
2026-05-30 13:05:13 +0800

    更改随机填充

--- src/pal.rs
+++ src/pal.rs
@@ -12,9 +12,10 @@ extern "C" fn clock_gettime(clock_id: clockid_t, tp: *mut timespec) -> c_int {
 #[cfg_attr(feature = "linkage_weak", linkage = "weak")]
 extern "C" fn esp_fill_random(buf: *mut c_void, len: usize) {
     unsafe {
+        static mut COUNT: u8 = 0;
         for i in std::slice::from_raw_parts_mut::<u8>(buf as *mut u8, len) {
-            let v = cortex_m::peripheral::SYST::get_current().to_le_bytes();
-            *i = v[0] ^ v[1] ^ v[2] ^ v[3];
+            *i = COUNT;
+            COUNT += 1;
         }
     }
 }
